c57db5e44896ac9e1785718418dc5297e99372f5
Danis96 <danis.preldzic@gmail.com>
2019-11-17 00:31:37 +0100

    Updates

---
+++ precache-manifest.051a9442fed13e67f9c78fffe8608c6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e17ed99001cd88f716df563a9fa2215",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "08f653fe3465c2c45635",
+    "url": "/weatherapp/static/css/2.01dc1de8.chunk.css"
+  },
+  {
+    "revision": "66b9f902736323432662",
+    "url": "/weatherapp/static/css/main.ce696560.chunk.css"
+  },
+  {
+    "revision": "08f653fe3465c2c45635",
+    "url": "/weatherapp/static/js/2.e778b518.chunk.js"
+  },
+  {
+    "revision": "66b9f902736323432662",
+    "url": "/weatherapp/static/js/main.b5af2fd0.chunk.js"
+  },
+  {
+    "revision": "cd586449e117cbbf3346",
+    "url": "/weatherapp/static/js/runtime-main.d198a0ad.js"
+  },
+  {
+    "revision": "bf472238161058594beffb054b8e7958",
+    "url": "/weatherapp/static/media/147137.bf472238.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
